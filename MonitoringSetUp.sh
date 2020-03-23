8c988260a52fa433539219a955d4b242c4efe032
nehsharmMS <60141664+nehsharmMS@users.noreply.github.com>
2020-03-23 20:12:20 +0530

    Update MonitoringSetUp.sh

--- MonitoringSetUp.sh
+++ MonitoringSetUp.sh
@@ -29,9 +29,9 @@ sudo apt install apt-transport-https ca-certificates curl gnupg2 software-proper
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+#apt-cache policy docker-ce
+#sudo apt install docker-ce -y
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 echo -e "\n\n###################################### Logging into ACR and Pulling Monitoring Image ###########################\n\n"
 
