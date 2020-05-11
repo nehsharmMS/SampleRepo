e04e8c7e8f30bb5ccb923da716c0caaea2e566f9
Ankit Arora <56643114+ankit-arora12@users.noreply.github.com>
2020-05-11 05:51:08 +0530

    Create ReplicationStatus.sh

---
+++ ReplicationStatus.sh
@@ -0,0 +1,35 @@
+#get inputs from args
+while getopts ":t:g:r:" opt; do
+  case $opt in
+    t) tenant="$OPTARG"
+    ;;
+    g) genevaAccount="$OPTARG"
+    ;;
+    r) roleInstance="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+echo "$PATH"
+echo "$tenant"
+echo "$genevaAccount"
+echo "$roleInstance"
+
+ghe-repl-status
+
+if [ $? -eq 0 ]
+then
+        date
+        echo "Sending 0"
+        echo '{"Account":"$genevaAccount","Namespace":"GHPIMetrics","Metric":"ReplicationStatus","Dims":{"Tenant":"$tenant","Role":"GHPI","Role_Instance":"$roleInstance"}}:0|g' | socat -t 1 - UDP-SENDTO:127.0.0.1:8126
+
+else
+      date
+       echo "Sending 1"
+       echo '{"Account":"$genevaAccount","Namespace":"GHPIMetrics","Metric":"ReplicationStatus","Dims":{"Tenant":"$tenant","Role":"GHPI","Role_Instance":""}}:1|g' | socat -t 1 - UDP-SENDTO:127.0.0.1:8126
+
+fi
