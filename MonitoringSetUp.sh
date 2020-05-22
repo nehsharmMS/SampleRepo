eb7263c77e30a01fe6a7327afc12595602f0282b
Vineeth Hanumanth <39301309+hvinett@users.noreply.github.com>
2020-05-22 21:32:12 +0530

    added env variables to automate pipeline

--- MonitoringSetUp.sh
+++ MonitoringSetUp.sh
@@ -1,38 +1,155 @@
 #!/bin/bash
 
-set -e
-
-## Fetch Monitoring Docker image from Azure Container Registry 
-while getopts ":t:u:p:r:v:f:n:e:a:" opt; do
-  case $opt in
-    t) tenant="$OPTARG"
-    ;;
-    u) username="$OPTARG"
-    ;;
-    p) password="$OPTARG"
-    ;;
-    r) monitoring_role="$OPTARG"
-    ;;
-    v) config_version="$OPTARG"
-    ;;
-    f) front_end_url="$OPTARG"
-    ;;
-    n) monitoring_namespace="$OPTARG"
-    ;;
-    e) monitoring_environment="$OPTARG"
-    ;;
-    a) monitoring_account="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+    case "$key" in
+    --tenant)  
+        tenant=$2
+        shift
+        shift
+        echo "Tenant: $tenant"
+        ;;
+    --username)  
+        username=$2
+        shift
+        shift
+        echo "username: $username"
+        ;;
+    --password)  
+        password=$2
+        shift
+        shift
+        echo "password: $password"
+        ;;
+    --monitoringRole)  
+        monitoring_role=$2
+        shift
+        shift
+        echo "monitoring role: $monitoring_role"
+        ;;
+    --configVersion)  
+        config_version=$2
+        shift
+        shift
+        echo "config version: $config_version"
+        ;;
+    --frontEndUrl)  
+        front_end_url=$2
+        shift
+        shift
+        echo "front end url: $front_end_url"
+        ;;
+    --monitoringNamespace)  
+        monitoring_namespace=$2
+        shift
+        shift
+        echo "monitoring namespace: $monitoring_namespace"
+        ;;
+    --monitoringEnvironment)  
+        monitoring_environment=$2
+        shift
+        shift
+        echo "monitoring environment: $monitoring_environment"
+        ;;
+    --monitoringAccount)  
+        monitoring_account=$2
+        shift
+        shift
+        echo "monitoring account: $monitoring_account"
+        ;;
+    --containerRegistry)  
+        container_registry=$2
+        shift
+        shift
+        echo "container registry: $container_registry"
+        ;;
+    --containerLabel)  
+        container_label=$2
+        shift
+        shift
+        echo "container label: $container_label"
+        ;;
+     --isReplica)
+        is_replica=$2
+        shift
+        shift
+        echo "is replica: $is_replica"
+	;;
+     *)
+        echo "Invalid parameter: $1"
+        exit 1
+        ;;
+    esac
 done
 
-if [[ -z "$tenant" || -z "$username" || -z "$password" || -z "$monitoring_role" || -z "$config_version" || -z "$front_end_url" || -z "$monitoring_namespace" || -z "$monitoring_environment" || -z "$monitoring_account"]]
-then
-  echo -e "\nError : All the above arguments like Tenant, ACR username , password etc.. are mandatory arguments.Please provide required arguments to setup monitoring pipeline.Exiting the script..."
-  exit 1
-else
+if [[ -z "$tenant" ]]
+then 
+    echo "\nError: Tenant is mandatory. Please provide Tenant to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$username" ]]
+then 
+    echo "\nError: username is mandatory. Please provide username to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$password" ]]
+then 
+    echo "\\nError: password is mandatory. Please provide password to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$monitoring_role" ]]
+then 
+    echo "\nError: monitoring role is mandatory. Please provide monitoring role to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$config_version" ]]
+then 
+    echo "\nError: config version is mandatory. Please provide config version to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$front_end_url" ]]
+then 
+    echo "\nError: front end url is mandatory. Please provide front end url to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[  -z "$monitoring_namespace" ]]
+then 
+    echo "\nError: monitoring namespace is mandatory. Please provide monitoring namespace to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$monitoring_environment" ]]
+then 
+    echo "\nError: monitoring environment is mandatory. Please provide monitoring environment to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$monitoring_account" ]]
+then 
+    echo "\nError: monitoring account is mandatory. Please provide monitoring account to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$container_registry" ]]
+then 
+    echo "\nError: container registry is mandatory. Please provide container registry to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
+if [[ -z "$container_label" ]]
+then 
+    echo "\nError: container label is mandatory. Please provide container label to setup monitoring pipeline. Exiting the script."
+    exit 1
+fi
+
   # Tenant=AzTenant
   echo -e "\n#################################### Monitoring Setup For **$tenant** ####################################\n\n"
   echo -e "###################################### Installing Docker and Azure CLI #########################################\n\n"
@@ -47,12 +164,9 @@ else
 
   echo -e "\n\n###################################### Logging into ACR and Pulling Monitoring Image ###########################\n\n"
 
-  ## sudo az acr login --name ghpiamecontainer --username $username -p $password
-  sudo az acr login --name ghmccontainer --username $username -p $password
-
-  ##sudo docker pull ghmccontainer.azurecr.io/monitor:latest 
-  ## sudo docker pull ghpiamecontainer.azurecr.io/monitor:latest
-  sudo docker pull ghmccontainer.azurecr.io/monitor_ghpi:latest
+  sudo az acr login --name $container_registry --username $username -p $password
+  container_name=$container_registry".azurecr.io/"$container_label":latest"
+  sudo docker pull $container_name
 
    echo -e "Converting pem file to cert and private key file...."
    GCS_CERT_FOLDER=/gcscerts
@@ -83,13 +197,21 @@ else
     echo -e "\n\n###################################### Creating Environment variable files for MDS and MDM #####################\n\n"
 
 echo "export FRONT_END_URL=$front_end_url" > EnvVariables.sh 
+
 sudo rm -f /tmp/collectd
 cat > /tmp/collectd <<EOT
 # Setting Environment variables for Monitoring
          
 export MONITORING_TENANT=$tenant
 export MONITORING_ROLE=$monitoring_role
-export MONITORING_ROLE_INSTANCE=${tenant}_1
+export MONITORING_ROLE_INSTANCE=${tenant}_primary
+
+if [ $is_replica = "true" ] || [ $is_replica = "True" ] ; then
+{
+	export MONITORING_ROLE_INSTANCE=${tenant}_replica
+}
+fi
+
 EOT
 
 MDSD_ROLE_PREFIX=/var/run/mdsd/default
@@ -127,7 +249,14 @@ cat > /tmp/mdsd <<EOT
     export MONITORING_USE_GENEVA_CONFIG_SERVICE=true
     export MONITORING_TENANT=$tenant
     export MONITORING_ROLE=$monitoring_role
-    export MONITORING_ROLE_INSTANCE=${tenant}_1
+    export MONITORING_ROLE_INSTANCE=${tenant}_primary
+    
+    if [ $is_replica = "true" ] || [ $is_replica = "True" ] ; then
+    {
+	    export MONITORING_ROLE_INSTANCE=${tenant}_replica
+    }
+    fi
+    
 EOT
 
 ## Run container using Monitoring image, if not running already. Copy above created env variable files to container and start the cron job on running container..
@@ -135,7 +264,7 @@ echo -e "Created env variables files for MDM and MDS\n"
 
 echo -e "\n\n###################################### Running and setting up container ########################################\n\n"
 
-MyContainerId="$(sudo docker ps -aqf "name=monitor")"
+MyContainerId="$(sudo docker ps -aqf "name=$container_label")"
 
 #echo $MyContainerId
 if [[ ! -z $MyContainerId ]]
@@ -144,9 +273,7 @@ echo -e "A container with id $MyContainerId is already running. Stopping the con
 sudo docker stop $MyContainerId
 fi
 
-
-## MyContainerId="$(sudo docker run -it --privileged --rm -d --network host --name monitor ghpiamecontainer.azurecr.io/monitor:latest)"
-MyContainerId="$(sudo docker run -it --privileged --rm -d --network host --name monitor_ghpi ghmccontainer.azurecr.io/monitor_ghpi:latest)"
+MyContainerId="$(sudo docker run -it --privileged --rm -d --network host --name $container_label $container_name)"
   if [[ -z $MyContainerId ]]
   then
     echo "Error : Failed to run monitor container.Exiting the script..."
@@ -172,7 +299,7 @@ MyContainerId="$(sudo docker run -it --privileged --rm -d --network host --name 
     sudo docker exec -itd $MyContainerId bash -c '/etc/init.d/cron start'
     
  echo -e "Setting up of Monitoring container is successful.\n"
-fi
+
 
 echo -e "Cleaning up certs and keys from the VM\n"
 
